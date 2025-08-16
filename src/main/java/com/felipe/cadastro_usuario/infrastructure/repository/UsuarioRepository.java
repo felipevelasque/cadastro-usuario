4c80c6669a923716e98d6d22ce1e14a365cffcb4
Felipe Velasque <fe.velasquee@gmail.com>
2025-08-16 17:52:31 -0300

    feat: add deleteByEmail method in UsuarioRepository

--- src/main/java/com/felipe/cadastro_usuario/infrastructure/repository/UsuarioRepository.java
+++ src/main/java/com/felipe/cadastro_usuario/infrastructure/repository/UsuarioRepository.java
@@ -1,10 +1,13 @@
 package com.felipe.cadastro_usuario.infrastructure.repository;
 
 import com.felipe.cadastro_usuario.infrastructure.entities.Usuario;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
     Optional<Usuario> findByEmail(String email);
 
+    @Transactional
+    void deleteByEmail(String email);
 }
