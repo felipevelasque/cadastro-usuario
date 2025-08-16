752dc68838e1857b7d9846af9305d51f0444e53f
Felipe Velasque <fe.velasquee@gmail.com>
2025-08-16 17:47:17 -0300

    feat: add findByEmail method in UsuarioRepository

--- src/main/java/com/felipe/cadastro_usuario/infrastructure/repository/UsuarioRepository.java
+++ src/main/java/com/felipe/cadastro_usuario/infrastructure/repository/UsuarioRepository.java
@@ -2,6 +2,9 @@ package com.felipe.cadastro_usuario.infrastructure.repository;
 
 import com.felipe.cadastro_usuario.infrastructure.entities.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
 
-public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    Optional<Usuario> findByEmail(String email);
+
 }
