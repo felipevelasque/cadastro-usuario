66d45d8a802ceb81aec7eafafb4b5df7415e354f
Felipe Velasque <fe.velasquee@gmail.com>
2025-08-18 23:05:33 -0300

    feat: add user request and response DTOs

---
+++ src/main/java/com/felipe/cadastro_usuario/dto/UsuarioResponseDTO.java
@@ -0,0 +1,6 @@
+package com.felipe.cadastro_usuario.dto;
+
+public record UsuarioResponseDTO(Long id,
+                                 String email,
+                                 String nome){
+}
